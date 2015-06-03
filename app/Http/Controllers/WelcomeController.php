729ac9d22081b6052c7ccd38f849d4e190a97dae
abougeniere <abougeniere@yahoo.fr>
2015-06-03 17:47:04 +0200

    bug datagrid 2

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -47,59 +47,24 @@ class WelcomeController extends Controller {
 
 
         $reader = Reader::createFromPath('../uploads/pcl.csv');
+
         $reader->setDelimiter(";");
-        //dd($reader);
-/*
-        foreach ($reader as $index => $row) {
-        echo ($row[0] ."<br>");
 
-            //do something meaningfull here with $row !!
-            //$row is an array where each item represent a CSV data cell
-            //$index is the CSV row index
-        }
-        /*
-//the $reader object will use the 'r+' open mode as no `open_mode` parameter was supplied.
-       // $writer = Writer::createFromPath(new SplFileObject('/path/to/your/csv/file.csv', 'a+'), 'w');
-//the $writer object open mode will be 'w'!!
+        $data = $reader->fetchAll();
 
-		return view('welcome');
-        //return View::make('hello', array('theLocation' =>  'NYC', 'theWeather'=> 'stormy', 'theColors' => $theColors));
-        */
-        //return View::make('welcome', array('datas' => $reader->getDelimiter()));
-        $test ="ça marche!";
-
-        //return view('welcome', ['name' => $test]);
-
-
-
-        $data = $reader
-            ->addFilter(function ($row) {
-                return "97812014M4S"!= $row[0]; //we are looking for the year 2010
-            })
-            ->setLimit(20) //we just want the first 20 results
-            ->fetchAll();
         //->toHTML('table-csv-data with-header');
         //dd($data);
 
         $headers = $reader->fetchOne();
         //dd($headers);
 
-        $dataset = DataSet::source($data)->paginate(10)->getSet();
+        $dataset = DataSet::source($data)->getSet();
         //dd($dataset);
 
-        //var_dump($dataset);
-        //return view('welcome',$dataset);
-
-        $grid = DataGrid::source($data)->getSet();
-
-
-
-        //$grid = DataGrid::source($data);
+        $grid = DataGrid::source($dataset);
         //dd($grid);
 
-$i=array(1,2,3);
-        return view('welcome',compact('grid'));
-        //return view('welcome',$grid);
+        return view('welcome',$grid);
 
 	}
 
